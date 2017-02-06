79b7490fcfc3746b93afaacbc6bebad778e8eb6d
Simon <3830106@gmail.com>
2017-02-07 00:29:28 +0200

    jumping, bug fixes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# FPS controller for Godot game engine
+# FPS controller template for Godot game engine
 
 My first attempt to create FPS controller for Godot engine. My goal is to have something similar what Unity has by default in their characters package.
+
+Using this for learning purposes. Done a lot of reverse engineering of examples I found on Internet.
--- scripts/hero.gd
+++ scripts/hero.gd
@@ -17,6 +17,15 @@ const ACCEL = 2
 const DEACCEL = 4
 const GRAVITY = -9.8 * 3
 
+const JUMP_SPEED = 3 * 3
+
+var on_floor = false
+
+const MAX_SLOPE_ANGLE = 40
+
+var jump_timeout = 0
+const MAX_JUMP_TIMEOUT = 0.2
+
 func _input(event):
 	if event.type == InputEvent.MOUSE_MOTION:
 		yaw = fmod(yaw - event.relative_x * view_sensitivity, 360)
@@ -39,16 +48,11 @@ func _exit_tree():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _fly(delta):
-	# Check if player clicked Escape button
 	if(Input.is_key_pressed(KEY_ESCAPE)):
-		# Check if player clicked Shift button
 		if(Input.is_key_pressed(KEY_SHIFT)):
-			# If both buttons are clicked quit the game
 			self.get_tree().quit()
 
-	# read the rotation of the camera
 	var aim = get_node("yaw/heroCamera").get_global_transform().basis
-	# calculate the direction where the player want to move
 	var direction = Vector3()
 	if Input.is_action_pressed("move_forward"):
 		direction -= aim[2]
@@ -61,17 +65,13 @@ func _fly(delta):
 	
 	direction = direction.normalized()
 		
-	# calculate the target where the player want to move
 	var target = direction*FLY_SPEED
 	
-	# calculate the velocity to move the player toward the target
 	velocity = Vector3().linear_interpolate(target, FLY_ACCEL * delta)
 	
-	# move the node
 	var motion = velocity*delta
 	motion = self.move(motion)
 	
-	# slide until it doesn't need to slide anymore, or after n times
 	var original_vel = velocity
 	var attempts = 4 # number of attempts to slide the node
 	
@@ -79,25 +79,24 @@ func _fly(delta):
 		var n = self.get_collision_normal()
 		motion = n.slide(motion)
 		velocity = n.slide(velocity)
-		# check that the resulting velocity is not opposite to the original velocity, which would mean moving backward.
+		
 		if(original_vel.dot(velocity) > 0):
 			motion = self.move(motion)
 			if (motion.length() < 0.001):
 				break
 		attempts -= 1
 
-# Just a modified copy of _fly function
 func _walk(delta):
-	# Check if player clicked Escape button
+	if jump_timeout > 0:
+		jump_timeout -= delta
+	var ray = get_node("ray")
+	
 	if(Input.is_key_pressed(KEY_ESCAPE)):
-		# Check if player clicked Shift button
 		if(Input.is_key_pressed(KEY_SHIFT)):
-			# If both buttons are clicked quit the game
 			self.get_tree().quit()
 
-	# read the rotation of the camera
 	var aim = get_node("yaw/heroCamera").get_global_transform().basis
-	# calculate the direction where the player want to move
+	
 	var direction = Vector3()
 	if Input.is_action_pressed("move_forward"):
 		direction -= aim[2]
@@ -108,48 +107,55 @@ func _walk(delta):
 	if Input.is_action_pressed("move_right"):
 		direction += aim[0]
 	
-	# reset the flag for hero’s movement state
 	is_moving = (direction.length() > 0) # new code
 	
+	direction.y = 0
 	direction = direction.normalized()
 	
-	# implementing gravity
-	velocity.y += delta * GRAVITY
+	var is_ray_colliding = ray.is_colliding()
+	
+	if !on_floor and jump_timeout <= 0 and is_ray_colliding:
+		self.set_translation(ray.get_collision_point())
+		on_floor = true
+	elif on_floor and not is_ray_colliding:
+		on_floor = false
+	
+	if on_floor:
+		var n = ray.get_collision_normal()
+		velocity = velocity - velocity.dot(n) * n
+
+		if (rad2deg(acos(n.dot(Vector3(0, 1, 0)))) > MAX_SLOPE_ANGLE):
+			velocity.y += delta * GRAVITY
+	else:
+		velocity.y += delta * GRAVITY
 	
 	
-	# calculate the target where the player want to move
 	var target = direction * WALK_MAX_SPEED
-	# if the character is moving, he must accelerate.
-	# Otherwise he deccelerates.
 	var accel = DEACCEL
 	if is_moving:
 		accel = ACCEL
 	
-	# calculate velocity’s change
 	var hvel = velocity
 	hvel.y = 0
-	# calculate the velocity to move toward the target,
-	# but only on the horizontal plane XZ
 	hvel = hvel.linear_interpolate(target, accel * delta)
 	velocity.x = hvel.x
 	velocity.z = hvel.z
 		
 	
-	# move the node
-	var motion = velocity*delta
+	var motion = velocity * delta
 	motion = self.move(motion)
 	
-	# slide until it doesn't need to slide anymore, or after n times
 	var original_vel = velocity
-	var attempts = 4 # number of attempts to slide the node
 	
-	while(attempts and self.is_colliding()):
+	if(motion.length() > 0 and self.is_colliding()):
 		var n = self.get_collision_normal()
 		motion = n.slide(motion)
 		velocity = n.slide(velocity)
-		# check that the resulting velocity is not opposite to the original velocity, which would mean moving backward.
-		if(original_vel.dot(velocity)>0):
+		if(original_vel.dot(velocity) > 0):
 			motion = self.move(motion)
-			if (motion.length() < 0.001):
-				break
-		attempts -= 1
+	
+	if on_floor:
+		if Input.is_action_pressed("jump"):
+			velocity.y = JUMP_SPEED
+			jump_timeout = MAX_JUMP_TIMEOUT
+			on_floor = false
